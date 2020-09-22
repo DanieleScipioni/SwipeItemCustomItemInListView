492cb6c3cf97791ed71a9cc57f3cb556fdb2c945
Daniele Scipioni <daniele.scipioni@gmail.com>
2020-09-22 13:45:42 +0200

    Add SwipeItemCustomItemInListView solution and project

---
+++ SwipeItemCustomItemInListView/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+
+namespace SwipeItemCustomItemInListView.AppShell
+{
+    sealed partial class App
+    {
+        public App()
+        {
+            InitializeComponent();
+            Suspending += OnSuspending;
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            var rootFrame = (MainPage)Window.Current.Content;
+
+            if (rootFrame == null)
+            {
+                rootFrame = new MainPage();
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Load state from previously suspended application
+                }
+
+                Window.Current.Content = rootFrame;
+            }
+
+            if (e.PrelaunchActivated == false)
+            {
+                Window.Current.Activate();
+            }
+        }
+
+        private static void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            deferral.Complete();
+        }
+    }
+}
---
+++ SwipeItemCustomItemInListView/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SwipeItemCustomItemInListView.AppShell
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
