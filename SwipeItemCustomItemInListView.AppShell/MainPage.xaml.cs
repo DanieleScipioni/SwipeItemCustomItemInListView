05505b2d227b670573d649baca0bec95ea222d73
Daniele Scipioni <daniele.scipioni@gmail.com>
2020-09-23 08:37:15 +0200

    Add code to reproduce issue

--- SwipeItemCustomItemInListView.AppShell/MainPage.xaml.cs
+++ SwipeItemCustomItemInListView.AppShell/MainPage.xaml.cs
@@ -1,9 +1,14 @@
-﻿namespace SwipeItemCustomItemInListView.AppShell
+﻿using System.Collections.Generic;
+
+namespace SwipeItemCustomItemInListView.AppShell
 {
     public sealed partial class MainPage
     {
+        private readonly List<string> _list;
+
         public MainPage()
         {
+            _list = new List<string> {"line 1", "line 2"};
             InitializeComponent();
         }
     }
